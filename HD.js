062dc80d229c4d878d98ecc6a64e7ce0757186e9
liuzhihua <liuzhihuarose@126.com>
2021-12-12 00:14:19 +0800

    声明promise并绑定this

--- HD.js
+++ HD.js
@@ -1,1 +1,20 @@
-console.log("测试链接js文件")
+class HD {
+    static PENDING = "pending";
+    static FUFILLED = "fufilled";
+    static REJECTED = "rejected";
+
+    constructor(executor) {
+        this.status = HD.PENDING;
+        this.value = null;
+        executor(this.resolve.bind(this), this.rejected.bind(this))
+    }
+
+    resolve(value) {
+this.status=HD.FUFILLED;
+this.value=value;
+    }
+    rejected(reason) {
+this.status=HD.REJECTED;
+this.value=reason;
+    }
+}
